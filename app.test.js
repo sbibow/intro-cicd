958d30c41709c0706539f570d33730ef07b4d675
Semjon Bibow <semjon.bibow@gmail.com>
2019-01-07 19:05:48 +0100

    Use describe/it rather than test

--- app.test.js
+++ app.test.js
@@ -1,7 +1,13 @@
 const request = require("supertest")
 const app = require("./app")
 
-test("Responds with hello world", async () => {
-  const response = await request(app).get("/?a=1&b=4")
-  expect(response.body).toEqual({sum: 5})
+describe("Home route", () => {
+  it("Returns sum on integers" , async () => {
+    const response = await request(app).get("/?a=1&b=4")
+    expect(response.body).toEqual({sum: 5})
+  })
+  it("Returns null on non-integers", async () => {
+    const response = await request(app).get("/?a=a&b=b")
+    expect(response.body).toEqual({sum: null})
+  })
 })
